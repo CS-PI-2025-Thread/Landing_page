9299099528ff2e8960769cb681c0b2617b9c1156
larybino <binolaryssa@gmail.com>
2025-03-31 15:10:32 -0300

    feat(landing-page): adiciona modo contraste

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function closeMenu() {
     const menu = document.querySelector('.menu-mobile');
     menu.classList.remove('active');
 }
+
+function toggleContrast() {
+  const htmlElement = document.documentElement;
+  const currentTheme = htmlElement.getAttribute('data-theme');
+  const targetTheme = currentTheme === 'contrast' ? null : 'contrast';
+
+  htmlElement.setAttribute('data-theme', targetTheme);
+}
